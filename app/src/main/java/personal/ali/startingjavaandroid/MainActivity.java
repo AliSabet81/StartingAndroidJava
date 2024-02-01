7d75c1fc27b0de02610868576b094d3d5482f993
AliSabet81 <sabet.farshid1381@gmail.com>
2024-02-02 02:45:36 +0330

    first commit

---
+++ app/src/main/java/personal/ali/startingjavaandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package personal.ali.startingjavaandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
